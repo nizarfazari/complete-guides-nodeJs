266836a26ba689956074ae9b33be4497f446615b
Muhammad Nizar Fazari <nexsel32@gmail.com>
2023-09-30 22:58:37 +0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const path = require("path");
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+// to set view engine, there is many view engine => pug,handlebars,ejs
+app.set("view engine", "ejs");
+
+//to set view location in where is it
+app.set("views", "views");
+
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+const { get404 } = require("./controllers/error");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
+
+app.use(get404);
+
+app.listen(3000);
