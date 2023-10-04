9563e48450602262333c7af1a51fa05c8de9ac53
Muhammad Nizar Fazari <nexsel32@gmail.com>
2023-10-04 23:27:53 +0700

    enchanching the app
    - make per module
    - prepare for crud

--- routes/shop.js
+++ routes/shop.js
@@ -1,9 +1,20 @@
 const express = require("express");
 
-const { getProduct } = require("../controllers/product");
+const {
+  getProduct,
+  getIndex,
+  getCart,
+  getCheckout,
+  getOrders,
+} = require("../controllers/shop");
 
 const router = express.Router();
 
-router.get("/", getProduct);
+router.get("/", getIndex);
+
+router.get("/products", getProduct);
+router.get("/cart", getCart);
+router.get("/checkout", getCheckout);
+router.get("/checkout", getOrders);
 
 module.exports = router;
